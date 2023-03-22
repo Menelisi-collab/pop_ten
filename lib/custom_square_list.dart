0185af9a8b109e3f06b6648e0ffe9effac420662
Menelisi-collab <mitjho@gmail.com>
2023-03-22 15:01:57 +0200

    fourth commit

---
+++ lib/custom_square_list.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:pop_ten/custom_square.dart';
+
+List<int> customSquares = List.generate(101, (index) => index, growable:  false)..shuffle();
+
+var i=0;
+
+
+
+// create function to populate customSquares
+// void generateRandomNumber() {
+//   // generate random number
+//   final int currentRandomNumber = Random().nextInt(121);
+//
+//   // feed number to custom square
+//   final CustomSquare theCustomSquare = CustomSquare(value: currentRandomNumber);
+//
+//   // add custom square to list customSquares
+//   customSquares.add(theCustomSquare);
+//
+// }
+
+// loop 120 times, calling generateRandomNumber function each time
+void loopGenerator(){
+  while (1 <=120){
+    print(i);
+    i ++;
+  }
+
+}
+
+
+
+// feed the customSquares to Gridview.builder
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pop_ten/custom_square.dart';
+import 'package:pop_ten/custom_square_list.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,52 +12,62 @@ class _HomePageState extends State<HomePage> {
   static int numberInRow = 11;
   int numberOfSquares = numberInRow * 11;
 
-  void main(){
-    List<int> numbers = [];
-    numbers.shuffle();
-  }
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: Colors.purple,
       body: Column(
         children: [
           Expanded(
             flex: 5,
             child: Container(
               child: GridView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: numberOfSquares,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: customSquares.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: numberInRow),
                   itemBuilder: (BuildContext context, int index) {
+
+                    final currentNumber = customSquares[index];
+
                     return Padding(
                       padding: const EdgeInsets.all(1.0),
                       child: Container(
-                        color: Colors.grey,
-                        child: Center(child: Text(index.toString())),
+                        color: Colors.white,
+                        child: Center(
+                          child: CustomSquare(value: currentNumber),
+                        ),
                       ),
                     );
                   }),
             ),
           ),
+
           Expanded(
             child: Container(
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Score: ',
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  AnimatedContainer(duration: const Duration(seconds: 20),
+                  child: Lottie.network('https://assets1.lottiefiles.com/temp/lf20_At0drg.json',),
                   ),
-                  Text('Play', style: TextStyle(color: Colors.white, fontSize: 30),)
+                  AnimatedContainer(duration: const Duration(seconds: 20),
+                    child: Lottie.network('https://assets1.lottiefiles.com/private_files/lf30_t0igqye8.json',),
+                  ),
+                  AnimatedContainer(duration: const Duration(seconds: 20),
+                    child: Lottie.network('https://assets6.lottiefiles.com/private_files/lf30_es53gnxq.json',),
+                  ),
+                  AnimatedContainer(duration: const Duration(seconds: 20),
+              child: Lottie.network('https://assets10.lottiefiles.com/packages/lf20_oCJ11v.json',),)
                 ],
               ),
             ),
           ),
+          Expanded(
+            child: SizedBox(
+              child: Text('Score: ', style: TextStyle(color: Colors.white, fontSize: 30, height: 2),),
+            ),
+          )
         ],
       ),
     );
